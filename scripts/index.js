07e259f113a599b6f572d8a2d69c0f2b27cfb76a
AitorLas <militancias@hotmail.es>
2023-05-04 00:08:52 +0100

    hora

--- scripts/index.js
+++ scripts/index.js
@@ -9,55 +9,50 @@
   /***** Footer (secció contacte) *****/
   afegeixHoraActual();
 
-
   /***** Oferta gastronòmica *****/
   function afegeixEsdevenimentCopiaText() {
-  document.getElementById("oferta-gastronomica").addEventListener("copy", function(event) {
-    alert("Solicite a info@restaurantorenga.cat la oferta gastronómica en formato digital y le enviaremos un documento PDF con toda la información.");
-  }) 
-  };
-
+    document
+      .getElementById("oferta-gastronomica")
+      .addEventListener("copy", function (event) {
+        alert(
+          "Solicite a info@restaurantorenga.cat la oferta gastronómica en formato digital y le enviaremos un documento PDF con toda la información."
+        );
+      });
+  }
 
   /***** Reserves *****/
   function afegeixEsdevenimentCanviNumeroComensals() {
-    const inputComensales = document.getElementById("comensales"); 
+    const inputComensales = document.getElementById("comensales");
     const outputPreuAproximat = document.getElementById("preuAproximat");
-    inputComensales.addEventListener("change", function(event) {
+    inputComensales.addEventListener("change", function (event) {
       const PRECIO_MEDIO_EUROS = 50;
-      outputPreuAproximat.textContent = PRECIO_MEDIO_EUROS * inputComensales.valueAsNumber;
-    }) 
-
+      outputPreuAproximat.textContent =
+        PRECIO_MEDIO_EUROS * inputComensales.valueAsNumber;
+    });
   }
 
   function afegeixEsdevenimentEnviamentFormulari() {
     const reservas = document.getElementById("reservas");
-    reservas.addEventListener('submit', function(event) {
-
+    reservas.addEventListener("submit", function (event) {
       event.preventDefault();
 
       const comensalesMenores = document.getElementById("edad").value;
       const salaSeleccionada = document.getElementById("salas").value;
-  
 
       if (comensalesMenores === "Si" && salaSeleccionada === "Sala México") {
-  
-        alert("No es posible hacer una reserva si hay menores de 5 años y la ubicación elegida es la Sala México, ya que no está suficientemente habilitada.");
+        alert(
+          "No es posible hacer una reserva si hay menores de 5 años y la ubicación elegida es la Sala México, ya que no está suficientemente habilitada."
+        );
       } else {
-     
         reservas.submit();
       }
     });
-
   }
-
-    
-  
-
 
   /***** Footer (secció contacte) *****/
   function afegeixHoraActual() {
     const nouParagraf = document.createElement("p");
-    nouParagraf.innerHTML = "Hora actual: <span id=\"horaActual\"></span>";
+    nouParagraf.innerHTML = 'Hora actual: <span id="horaActual"></span>';
 
     const elementAddress = document.querySelector("address");
     const ultimParagraf = elementAddress.lastElementChild;
@@ -77,16 +72,14 @@
     hores = estableixFormatDosDigits(hores);
     minuts = estableixFormatDosDigits(minuts);
     segons = estableixFormatDosDigits(segons);
-    document.getElementById("horaActual").innerHTML = hores + ":" + minuts + ":" + segons;
+    document.getElementById("horaActual").innerHTML =
+      hores + ":" + minuts + ":" + segons;
   }
 
   function estableixFormatDosDigits(valor) {
-   
-      if (valor < 10) {
-        return "0" + valor;
-      }
-      return valor;
+    if (valor < 10) {
+      return "0" + valor;
     }
-    
+    return valor;
   }
-)();
+})();
