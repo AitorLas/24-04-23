810e3b67ca3aacfc2facd087d2c2e8f3d9a96955
AitorLas <militancias@hotmail.es>
2023-05-03 20:45:02 +0100

    intento fallido de las reservas

--- scripts/index.js
+++ scripts/index.js
@@ -30,8 +30,45 @@
   }
 
   function afegeixEsdevenimentEnviamentFormulari() {
-    // Afegeix aquí el teu codi
+   
+      const reservas = document.getElementById("reservas");
+      reservas.addEventListener('submit', function(event) {
+
+        event.preventDefault();
+
+        const comensalesMenores = document.getElementById("edad").value;
+        const salaSeleccionada = document.getElementById("salas").value;
+    
+
+        if (comensalesMenores === "Si" && salaSeleccionada === "Sala México") {
+    
+          alert("No es posible hacer una reserva si hay menores de 5 años y la ubicación elegida es la Sala México, ya que no está suficientemente habilitada.");
+        } else {
+       
+          reservas.submit();
+        }
+      });
+    }
+    
+    // Agrega el listener al evento submit del formulario
+    afegeixEsdevenimentEnviamentFormulari();
+    
+
   }
+    /*const  seccionReservas = document.getElementById("reservas");
+    const respuestaEdad = seccionReservas.getElementById("edad").value;
+    const salaSeleccionada = document.getElementById('salas').value;
+
+      if (respuestaEdad === 'Si' && salaSeleccionada === 'Sala México') {
+        event.preventDefault();
+        alert('Texto de la ventana emergente');
+      }
+    }
+    
+    const formulario = document.querySelector('form');
+    formulario.addEventListener('submit', comprobarReserva);*/
+    
+  
 
 
   /***** Footer (secció contacte) *****/
